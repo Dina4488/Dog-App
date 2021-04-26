5127ede9c618d1a47807d87af171df141b0c8977
Dina Lewkowicz <dina4488@walla.co.il>
2021-04-26 13:20:02 +0300

    initialization

---
+++ src/model/DogModel.js
@@ -0,0 +1,9 @@
+class DogModel {
+    
+    constructor(dogName, img) {      
+        this.dogName = dogName;
+        this.img = img;
+    }    
+}
+
+export default DogModel;
---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Carousel, Container } from 'react-bootstrap';
+
+class HomePage extends Component {
+    render() {
+        return (
+            <div>
+                 <Container>
+                    
+                </Container>
+            </div>
+        );
+    }
+}
+
+export default HomePage;
